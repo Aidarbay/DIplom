29049057351eada8b245f8c5b4ecff8a135408e9
Aidar K <aidarkozuev2204@gmail.com>
2020-04-25 14:26:47 +0600

    added new files

---
+++ diplom/src/main/java/OmGU/IMIT/Main/Matrix.java
@@ -0,0 +1,13 @@
+package OmGU.IMIT.Main;
+
+public class Matrix {
+    private boolean [][] matrix;
+    private int size;
+
+    public Matrix(boolean [][] matrix, int resize) {
+        this.size = resize;
+        this.matrix = matrix;
+    }
+
+
+}
