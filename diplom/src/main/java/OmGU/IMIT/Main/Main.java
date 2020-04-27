6646ba87f760355a25050891ca51e20e89fbb00f
Aidar K <aidarkozuev2204@gmail.com>
2020-04-27 11:06:32 +0600

    modified version

--- diplom/src/main/java/OmGU/IMIT/Main/Main.java
+++ diplom/src/main/java/OmGU/IMIT/Main/Main.java
@@ -6,14 +6,14 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = {{1, 0, 1}, {0, 1, 0}, {0, 1, 1}};
+        int[][] matrix = {{1, 1, 0}, {0, 0, 1}, {0, 1, 1}};
         Matrix matrix1 = new Matrix(matrix, 3);
-        System.out.println(matrix1.toString());
         Random rand = new Random();
         List<Matrix> group = new ArrayList<Matrix>();
-        List<Matrix> sopryaj = new ArrayList<Matrix>();
-        boolean flag = false;
-        while (!flag) {
+        List<Matrix> firstConjugate = new ArrayList<Matrix>();
+        List<Matrix> secondConjugate = new ArrayList<>();
+
+        while (group.size() != 168) {
             int[][] matrix2 = {{rand.nextInt(2), rand.nextInt(2), rand.nextInt(2)},
                     {rand.nextInt(2), rand.nextInt(2), rand.nextInt(2)},
                     {rand.nextInt(2), rand.nextInt(2), rand.nextInt(2)}};
@@ -22,25 +22,26 @@ public class Main {
                 group.add(other);
                 //System.out.println(other.toString());
             }
-            if (group.size() == 168) {
-                flag = true;
-            }
         }
         for (Matrix h :
                 group) {
-            Matrix newMatrix = Matrix.umnojit(Matrix.umnojit(h,matrix1), h);
-            if(!sopryaj.contains(newMatrix)) {
-                   sopryaj.add(newMatrix);
-                   //System.out.println(h.toString());
+            Matrix newMatrix = Matrix.multiply(Matrix.multiply(h.transpose(), matrix1), h.inversion());
+            if (!firstConjugate.contains(newMatrix)) {
+                firstConjugate.add(newMatrix);
+                //System.out.println(h.toString());
             }
         }
-        System.out.println(sopryaj.size());
+        System.out.println(firstConjugate.size());
 
         for (Matrix h :
                 group) {
-            if(!sopryaj.contains(h)) {
+            if (!firstConjugate.contains(h)) {
+                secondConjugate.add(h);
                 System.out.println(h.toString());
             }
         }
+        System.out.println(secondConjugate.size());
+
+
     }
 }
--- diplom/src/main/java/OmGU/IMIT/Main/Matrix.java
+++ diplom/src/main/java/OmGU/IMIT/Main/Matrix.java
@@ -12,6 +12,16 @@ public class Matrix {
         this.matrix = matrix;
     }
 
+    Matrix(Matrix oldMatrix) {
+        size = oldMatrix.getSize();
+        matrix  = new int[oldMatrix.getSize()][oldMatrix.getSize()];
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                matrix[i][j] = oldMatrix.getElement(i,j);
+            }
+        }
+    }
+
     int determinant() {
         return (matrix[0][0] * matrix[1][1] * matrix[2][2] + matrix[0][1] * matrix[1][0] * matrix[2][2] +
                 matrix[0][0] * matrix[2][1] * matrix[1][2] + matrix[0][2] * matrix[1][1] * matrix[2][0] +
@@ -19,22 +29,79 @@ public class Matrix {
 
     }
 
-    static Matrix umnojit(Matrix matrix1, Matrix matrix2) {
+    static Matrix multiply(Matrix matrix1, Matrix matrix2) {
         int[][] newMatrix = new int[3][3];
         for (int i = 0; i < matrix1.getSize(); i++) {
             for (int j = 0; j < matrix2.getSize(); j++) {
-                newMatrix[i][j] = (matrix1.getElement(i, 0) * matrix2.getElement(0, j) +
-                        matrix1.getElement(i, 1) * matrix2.getElement(1, j) +
-                        matrix1.getElement(i, 2) * matrix2.getElement(2, j)) % 2;
+                newMatrix[i][j] = ((matrix1.getElement(i, 0) * matrix2.getElement(0, j)) +
+                        (matrix1.getElement(i, 1) * matrix2.getElement(1, j)) +
+                        (matrix1.getElement(i, 2) * matrix2.getElement(2, j))) % 2;
             }
         }
         return new Matrix(newMatrix, 3);
     }
 
+    Matrix inversion() {
+        int[][] E = new int[size][size];
+        Matrix newMatrix = new Matrix(this);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                E[i][j] = 0;
+
+                if (i == j) {
+                    E[i][j] = 1;
+                }
+            }
+        }
+        int index = 0;
+        int element;
+        for (int k = 0; k < size; k++) {
+
+            for (int j = 0; j < size; j++) {
+                if (newMatrix.getElement(j,k) == 1) {
+                    index = j;
+                }
+            }
+            if (k != index && newMatrix.getElement(k,k) != 1) {
+                for (int j = 0; j < size; j++) {
+                    element = newMatrix.getElement(k,j);
+                    newMatrix.setElement(k,j, newMatrix.getElement(index,j));
+                    newMatrix.setElement(index,j, element);
+                    element = E[k][j];
+                    E[k][j] = E[index][j];
+                    E[index][j] = element;
+                }
+            }
+            for (int i = 0; i < size; i++) {
+                if (i != k && newMatrix.getElement(i,k) == 1) {
+                    for (int j = 0; j < size; j++) {
+                        newMatrix.setElement(i,j,(newMatrix.getElement(i,j) + newMatrix.getElement(k,j)) % 2);
+                        E[i][j] = (E[i][j] + E[k][j]) % 2;
+                    }
+                }
+            }
+
+
+        }
+
+        return new Matrix(E, 3);
+    }
+
+    Matrix transpose() {
+        int[][] trans = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                trans[j][i] = matrix[i][j];
+            }
+        }
+        return new Matrix(trans, 3);
+    }
+
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder("");
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 str.append(matrix[i][j]).append(" ");
             }
@@ -45,10 +112,10 @@ public class Matrix {
 
     @Override
     public boolean equals(Object o) {
-        Matrix matrix1 = (Matrix)o;
+        Matrix matrix1 = (Matrix) o;
         for (int i = 0; i < size; i++) {
-            for (int j =0; j < size; j++) {
-                if(matrix[i][j] != matrix1.getElement(i,j)) {
+            for (int j = 0; j < size; j++) {
+                if (matrix[i][j] != matrix1.getElement(i, j)) {
                     return false;
                 }
             }
@@ -69,5 +136,9 @@ public class Matrix {
 
     private int getElement(int i, int j) {
         return matrix[i][j];
+    }
+
+    private void setElement(int i, int j, int element) {
+        matrix[i][j] = element;
     }
 }
