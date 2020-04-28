ef8b2b66fced73ec93c4d4d63ab28316d6944e9a
Aidar K <aidarkozuev2204@gmail.com>
2020-04-28 16:41:09 +0600

    new modification

--- Diplom/src/main/java/OmGU/IMIT/Main.java
+++ Diplom/src/main/java/OmGU/IMIT/Main.java
@@ -6,12 +6,15 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = {{0, 0, 1}, {1, 1, 0}, {0, 1, 1}};
+        int size = 0;
+        int[][] matrix = {{1, 1, 0}, {1, 1, 1}, {0, 1, 0}};
         Matrix matrix1 = new Matrix(matrix, 3);
         Random rand = new Random();
-        List<Matrix> group = new ArrayList<Matrix>();
+        List<Matrix> group = new ArrayList<>();
+
         List<Matrix> firstConjugate = new ArrayList<Matrix>();
-        List<Matrix> secondConjugate = new ArrayList<Matrix>();
+        List<Matrix> secondConjugate = new ArrayList<>();
+        List<List<Matrix>> conjugate = new ArrayList<>();
 
         while (group.size() != 168) {
             int[][] matrix2 = {{rand.nextInt(2), rand.nextInt(2), rand.nextInt(2)},
@@ -23,25 +26,27 @@ public class Main {
                 //System.out.println(other.toString());
             }
         }
-        for (Matrix h :
-                group) {
-            Matrix newMatrix = Matrix.multiply(Matrix.multiply(h.transpose(), matrix1), h.inversion());
-            if (!firstConjugate.contains(newMatrix)) {
-                firstConjugate.add(newMatrix);
-                //System.out.println(h.toString());
-            }
-        }
-        System.out.println(firstConjugate.size());
 
-        for (Matrix h :
-                group) {
-            if (!firstConjugate.contains(h)) {
-                secondConjugate.add(h);
-                System.out.println(h.toString());
+        while (size < 168) {
+            firstConjugate.clear();
+            for (Matrix h :
+                    group) {
+                Matrix newMatrix = Matrix.multiply(Matrix.multiply(h.transpose().inversion(), matrix1), h.inversion());
+                if (!firstConjugate.contains(newMatrix)) {
+                    firstConjugate.add(newMatrix);
+                }
             }
-        }
-        System.out.println(secondConjugate.size());
+            size += firstConjugate.size();
+            System.out.println(firstConjugate.size());
+            conjugate.add(firstConjugate);
 
+            for (Matrix h :
+                    group) {
+                if (!firstConjugate.contains(h)) {
+                }
+            }
+            System.out.println(secondConjugate.size());
+        }
 
     }
 }
