a2cd1a064c6bd93ff715b2db4caf53e8e9dcc353
Aidar K <aidarkozuev2204@gmail.com>
2020-06-27 12:24:53 +0600

    little modification HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-<<<<<<< HEAD
-        System.out.println("Hello, World from branch1");
-=======
         System.out.println("this is Hello, World from branch2");
->>>>>>> ak/lesson-one-second-branch-to-merge
     }
 }
